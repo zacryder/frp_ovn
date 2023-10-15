49c66df323a07b15904cce996e8b93fdb013c273
zack-orten <43951801+zacryder@users.noreply.github.com>
2023-10-15 20:27:30 +0800

    Update frp_openvpn.sh

--- frp_openvpn.sh
+++ frp_openvpn.sh
@@ -29,7 +29,7 @@ $GREEN####################################################$END
 
 Internet_Server(){
 OS=` awk -F= '/^NAME/{print $2}' /etc/os-release | sed "s/\"//g"`
-FRPURL=https://gitee.com/zack-orten/frp_ovn/raw/master/frp_0.52.1_linux_amd64.tar.gz
+FRPURL=https://ghproxy.com/https://github.com/fatedier/frp/releases/download/v0.52.1/frp_0.52.1_linux_amd64.tar.gz
 DIRNAME=frp_0.52.1_linux_amd64
 if [[ "$OS" == "Ubuntu" ]];then
       :
@@ -545,7 +545,7 @@ verb 3" > /etc/openvpn/server/client-common.txt
 	#安装frpc	
 	
 Install_frpc(){
-FRPURL=https://gitee.com/zack-orten/frp_ovn/raw/master/frp_0.52.1_linux_amd64.tar.gz
+FRPURL=https://ghproxy.com/https://github.com/fatedier/frp/releases/download/v0.52.1/frp_0.52.1_linux_amd64.tar.gz
 DIRNAME=frp_0.52.1_linux_amd64
 if [[ "$os" == "ubuntu" ]];then
       :
